6ad00ebed22a25d5b3cac23b215408f372a2c9db
Victoria-Vladimirova <victoria@MacBook-Air-Victoria.local>
2016-03-03 21:37:55 +0500

    Исправлены замечания
    добавлены тесты
    jsdoc
    порядок вызова методов в запросе

--- multivarka.js
+++ multivarka.js
@@ -1,97 +1,219 @@
 'use strict';
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
 
-var multivarka = {
+/**
+ * Метод для задания URL сервера, одновременно является точкой входа в билдер запросов
+ * @param url url для подключения
+ * @param client Опциональный клиент mongodb, используется для возможности подмены в тестах
+ * @returns {QueryBuilder} новый объект QueryBuilder
+ */
+module.exports.server = function (url, client) {
+    return new QueryBuilder(url, client);
+};
 
-    server: function (url) {
-        this.url = url;
-        return this;
-    },
 
-    collection: function (collection) {
-        this.collectionName = collection;
-        return this;
-    },
+/**
+ * Строит запрос
+ * @param url url для подключения
+ * @param client Опциональный клиент mongodb, используется для возможности подмены в тестах
+ * @constructor
+ */
+var QueryBuilder = function (url, client) {
 
-    where: function (field) {
-        this.field = field;
-        return this;
-    },
+    var builder = this;
 
-    not: function () {
-        this.isNot = true;
-        return this;
-    },
+    client = client || MongoClient;
 
-    _setCondition: function (value, ifFalse, ifTrue) {
-        var tempObj = {};
-        tempObj[this.field] = this.isNot ? ifFalse : ifTrue;
-        this.condition = tempObj;
-        return this;
-    },
+    this._url = url;
 
-    lessThan: function (value) {
-        return this._setCondition(value, {$gt: value}, {$lt: value});
-    },
+    /**
+     * Задает имя коллекции в MongoDB
+     * @param collection имя коллекции в MongoDB
+     * @returns {ActionStep} новый объект ActionStep - следующий шаг построения запроса
+     */
+    this.collection = function (collection) {
+        builder._collectionName = collection;
+        return new ActionStep(builder);
+    };
 
-    greatThan: function (value) {
-        return this._setCondition(value, {$lt: value}, {$gt: value});
-    },
-
-    equal: function (value) {
-        return this._setCondition(value, {$ne: value}, value);
-    },
-
-    include: function (list) {
-        return this._setCondition(list, {$nin: list}, {$in: list});
-    },
-
-    find: function (callback) {
-        return this._connect(callback, function (db, cb) {
-            db.collection(this.collectionName).find(this.condition).toArray(cb);
-        });
-    },
-
-    remove: function (callback) {
-        return this._connect(callback, function (db, cb) {
-            db.collection(this.collectionName).remove(this.condition, cb);
-        });
-    },
-
-    set: function (field, value) {
-        var tempObj = {};
-        tempObj[field] = value;
-        this.set = {$set: tempObj};
-        return this;
-    },
-
-    update: function (callback) {
-        return this._connect(callback, function (db, cb) {
-            db.collection(this.collectionName).update(this.condition || {}, this.set,
-                {multi: true}, cb);
-        });
-    },
-
-    insert: function (record, callback) {
-        return this._connect(callback, function (db, cb) {
-            db.collection(this.collectionName).insert(record, cb);
-        });
-    },
-
-    _connect: function (callback, action) {
-        MongoClient.connect(this.url, function (err, db) {
+    /**
+     * Подключение к MongoDB и выполнение action
+     * @param callback пользовательский коллбэк, переданный в метод действия (find/update/remove...)
+     * @param action функция, реализующая действие с выборкой
+     * @returns {QueryBuilder} билдер для связки
+     * @private
+     */
+    this._connect = function (callback, action) {
+        client.connect(builder._url, function (err, db) {
             if (err) {
                 callback(err);
             } else {
-                action.call(this, db, function (err, data) {
+                action.call(builder, db.collection(builder._collectionName), function (err, data) {
                     callback(err, data);
                     db.close();
                 });
             }
-        }.bind(this));
-        return this;
-    }
+        });
+        return builder;
+    };
 };
 
-module.exports = multivarka;
+/**
+ * Добавляет условие в запрос
+ * Содержит методы, которые могут идти после where
+ * @param builder билдер запроса
+ * @constructor
+ */
+var ConditionStep = function (builder) {
+
+    var where = this;
+
+    builder._isNot = false;
+
+    /**
+     * Устанавливает условие, с которым будет выполняться запрос в зависимости от наличия метода not
+     * @param negative условие, если был вызов not
+     * @param positive условие, если не было вызова not
+     * @returns {ActionStep} объект для следующего шага
+     * @private
+     */
+    var _setCondition = function (negative, positive) {
+        var condition = {};
+        condition[builder._field] = builder._isNot ? negative : positive;
+        builder._condition = condition;
+        return new ActionStep(builder);
+    };
+
+    /**
+     * Добавляет отрицание к следующему за этим оператором значению
+     * @returns {ConditionStep} текущий объект
+     */
+    this.not = function () {
+        builder._isNot = !builder._isNot;
+        return where;
+    };
+
+    /**
+     * Добавляет условие, что значение найденных записей будет меньше value
+     * @param value значение для условия сравнения
+     * @returns {ActionStep} объект для следующего шага
+     */
+    this.lessThan = function (value) {
+        return _setCondition({$gt: value}, {$lt: value});
+    };
+
+    /**
+     * Добавляет условие, что значение найденных записей будет больше value
+     * @param value значение для условия сравнения
+     * @returns {ActionStep} объект для следующего шага
+     */
+    this.moreThan = function (value) {
+        return _setCondition({$lt: value}, {$gt: value});
+    };
+
+    /**
+     * Добавляет условие, что значение найденных записей будет равно value
+     * @param value значение для условия сравнения
+     * @returns {ActionStep} объект для следующего шага
+     */
+    this.equal = function (value) {
+        return _setCondition({$ne: value}, value);
+    };
+
+    /**
+     * Добавляет условие, что в найденных записях будет поле из указанного набора полей
+     * @param list набор полей
+     * @returns {ActionStep} объект для следующего шага
+     */
+    this.include = function (list) {
+        return _setCondition({$nin: list}, {$in: list});
+    };
+};
+
+/**
+ * Добавляет в запрос действие, которое будет производится над коллекцией
+ * @param builder
+ * @constructor
+ */
+var ActionStep = function (builder) {
+
+    // проверяем, что ещё не было вызова where
+    if (!builder._field) {
+
+        /**
+         * Задает поле, по которому будет осуществляться сравнение
+         * разрешаем вызывать where только один раз в цепочке
+         * @param field поле для сравнения
+         * @returns {ConditionStep} объект для следующего шага — выбора условия
+         */
+        this.where = function (field) {
+            builder._field = field;
+            return new ConditionStep(builder);
+        };
+
+        /**
+         * Завершающее действие — добавляет запись в коллекцию
+         * Может быть вызван только если не было вызова where
+         * @param record запись, которая будет добавлена в коллекцию
+         * @param callback пользовательский коллбэк, куда передаются резульаты вставки
+         */
+        this.insert = function (record, callback) {
+            return builder._connect(callback, function (collection, cb) {
+                collection.insert(record, cb);
+            });
+        };
+
+    }
+
+    /**
+     * Задает поле и значение, которые будут обновлены на шаге UpdateStep
+     * @param field поле, которое будет обновлено
+     * @param value значение, которое будет присвоено полю
+     * @returns {UpdateStep} объект для следующего шага — вызова функции обновления
+     */
+    this.set = function (field, value) {
+        var set = {};
+        set[field] = value;
+        builder._set = {$set: set};
+        return new UpdateStep(builder);
+    };
+
+    /**
+     * Завершающее действие — поиск в коллекции по условию
+     * @param callback пользовательский коллбэк, куда передаются результаты запроса
+     */
+    this.find = function (callback) {
+        return builder._connect(callback, function (collection, cb) {
+            collection.find(builder._condition).toArray(cb);
+        });
+    };
+
+    /**
+     * Завершающее действие — удаление найденных записей из коллекции
+     * @param callback пользовательский коллбэк, куда передаются результаты удаления
+     */
+    this.remove = function (callback) {
+        return builder._connect(callback, function (collection, cb) {
+            collection.remove(builder._condition, cb);
+        });
+    };
+
+};
+
+/**
+ * Шаг билдера для обновления поля в записях коллекции
+ * @param builder билдер
+ * @constructor
+ */
+var UpdateStep = function (builder) {
+    /**
+     * Завершающее действие — обновление записи
+     * @param callback пользовательский коллбэк, куда передаются результаты обновления
+     * @returns {QueryBuilder} билдер
+     */
+    this.update = function (callback) {
+        return builder._connect(callback, function (collection, cb) {
+            collection.update(builder._condition || {}, builder._set, {multi: true}, cb);
+        });
+    };
+};
---
+++ test/test.js
@@ -0,0 +1,220 @@
+'use strict';
+
+var multivarka = require('./../multivarka.js');
+var assert = require('assert');
+var sinon = require('sinon');
+
+var petr = {
+    name: 'Пётр',
+    group: 'ПИ-302',
+    grade: 5
+};
+
+var clientAPI = function () {
+    return {
+
+        connect: function (url, callback) {
+            callback(null, this);
+        },
+
+        close: function () {
+        },
+
+        collection: function () {
+            return this;
+        },
+
+        find: function () {
+            return this;
+        },
+
+        remove: function (condition, callback) {
+            callback(null, []);
+        },
+
+        insert: function (condition, callback) {
+            callback(null, []);
+        },
+
+        update: function (condition, set, options, callback) {
+            callback(null, []);
+        },
+
+        toArray: function (callback) {
+            callback(null, []);
+        }
+
+    };
+};
+
+describe('Тестирование multivarka', function () {
+    it('Проверка equal', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').equal('КБ-301')
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({group: 'КБ-301'}).calledOnce);
+            });
+
+    });
+    it('Проверка not equal', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').not().equal('КБ-301')
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({group: {$ne: 'КБ-301'}}).calledOnce);
+            });
+    });
+    it('Проверка lessThan', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('grade').lessThan(4)
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({grade: {$lt: 4}}).calledOnce);
+            });
+
+    });
+    it('Проверка not lessThan', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('grade').not().lessThan(4)
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({grade: {$gt: 4}}).calledOnce);
+            });
+
+    });
+
+    it('Проверка not moreThan', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').not().moreThan(4)
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({group: {$lt: 4}}).calledOnce);
+            });
+
+    });
+    it('Проверка include', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').include(['ПИ-301', 'ПИ-302', 'КБ-301'])
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({group: {$in: ['ПИ-301', 'ПИ-302', 'КБ-301']}}).calledOnce);
+            });
+
+    });
+    it('Проверка not include', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var findSpy = sinon.spy(client, 'find');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').not().include(['ПИ-301', 'ПИ-302', 'КБ-301'])
+            .find(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(findSpy.withArgs({group: {$nin: ['ПИ-301', 'ПИ-302', 'КБ-301']}})
+                    .calledOnce);
+            });
+
+    });
+    it('Проверка remove', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var removeSpy = sinon.spy(client, 'remove');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').moreThan(4)
+            .remove(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(removeSpy.withArgs({group: {$gt: 4}}).calledOnce);
+            });
+    });
+    it('Проверка insert', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var insertSpy = sinon.spy(client, 'insert');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .insert(petr, function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(insertSpy.withArgs(petr).calledOnce);
+            });
+    });
+    it('Проверка set update', function () {
+
+        var client = clientAPI();
+        var connectSpy = sinon.spy(client, 'connect');
+        var collectionSpy = sinon.spy(client, 'collection');
+        var updateSpy = sinon.spy(client, 'update');
+
+        multivarka.server('fakeurl', client)
+            .collection('students')
+            .where('group').equal('ПИ-301')
+            .set('group', 'ПИ-302')
+            .update(function () {
+                assert(connectSpy.withArgs('fakeurl').calledOnce);
+                assert(collectionSpy.withArgs('students').calledOnce);
+                assert(updateSpy.withArgs({group: 'ПИ-301'}, {$set: {group: 'ПИ-302'}},
+                    {multi: true}).calledOnce);
+            });
+    });
+});
